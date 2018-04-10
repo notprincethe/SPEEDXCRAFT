959ef210f2e0f25d150d7a1f3324dc82680e4d09
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-10 19:23:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,22 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
+var prefix = '-';
+
+client.on('message', message => {
+    if(message.content === prefix + 'CR') {
+                         if(!message.channel.guild) return message.channel.send('**This Commnad only For Servers !**'); 
+         if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('**You Dont Have** `ADMINISTRATOR` **premission**').then(msg => msg.delete(6000))
+      message.guild.createRole({
+                  name: "Raimbo",
+                    color: "RANDOM",
+                    permissions: []
+     });
+
+       client.on('ready', () => {
+ setInterval(function(){roles.find('name', 'Raimbo').edit({color: 'RANDOM'}) 
+   },100);
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
