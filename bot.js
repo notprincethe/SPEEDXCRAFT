902a61676ec66a695d6d7bc0908c7922a8b0cfb3
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-22 12:22:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var request = require('request');
 
 client.on('ready', () => {
             client.user.setGame(`speedxcraft.com`,'https://www.youtube.com/?hl=FR');
@@ -9,6 +10,32 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong!');
   	}
+});
+
+
+var mcIP = 'play.speedxcraft.com'; 
+
+client.on('message', message => {
+    if (message.content === '/minecraft') {
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP ;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('فيه خطا ');
+            }
+            body = JSON.parse(body);
+            var status = '*السيرفر طافي*';
+            if(body.online) {
+                status = '**سيرفرك** **شغال**  -  ';
+                if(body.players.now) {
+                    status += '**' + body.players.now + '** واحد بالسيرفر';
+                } else {
+                    status += '*ولا حدا بلعب بالسيرفر*';
+                }
+            }
+            message.reply(status);
+        });
+    }
 });
 
 var prefix = "-";
