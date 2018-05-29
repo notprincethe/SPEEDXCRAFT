1e75fe729ff31cab0cac7ae795daf797780cf376
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-29 14:43:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,12 +13,6 @@ client.on('message', msg => {
   }
 });
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('369324885677309952').roles.find('name', 'rainbow').edit({color: 'RANDOM'}) // {By !M,S..❤#0254}
-    },100);
-});
-
  
 client.on('ready', () => {
     setInterval(function(){
