725c2c8deeafde792f4c5e385e2d05a0e3d6cb1e
ELMUSAUIDK <39737898+ELMUSAUIDK@users.noreply.github.com>
2018-07-07 14:12:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -228,34 +228,6 @@ client.on('message', message => {
 });
 
 
-client.on('message', message => {
-    var prefix = ".";
-
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-        let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-        if (message.content.split(' ')[0] == prefix + 'bc') {
-            if (!args[1]) {
-                message.channel.send("**-https://goo.gl/VddrDMbc <message>**");
-                return;
-            }
-            message.guild.members.forEach(m => {
-                if (!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                    .addField('» السيرفر :', `${message.guild.name}`)
-                    .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                    .addField(' » الرسالة : ', args)
-                    .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`, { embed: bc });
-            });
-        }
-    } else {
-        return;
-    }
-});
-
 
 client.on('message', message => {
 	    var prefix = ".";
