d85c63e3d0bf6b62f2dc5a52173ad468311f91d1
ELMUSAUIDK <39737898+ELMUSAUIDK@users.noreply.github.com>
2018-07-07 22:28:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>bcall')){
+ if (message.author.id !== '314845344313901057') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '314845344313901057') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
 });
 
 client.on('message', msg => {
