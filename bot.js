68cc4d2b523f9f6a984b92fdf134b7118f68ee4b
ELMUSAUIDK <39737898+ELMUSAUIDK@users.noreply.github.com>
2018-07-11 12:26:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -165,7 +165,7 @@ client.on("guildMemberAdd", member => {
 
 client.on('message', msg => {
 
-  if(msg.content.startsWith('.sug')) {
+  if(msg.content.startsWith('.suggest')) {
     if(!msg.channel.guild) return msg.reply('** هاذا الامر فقط للسيرفرات**');
     if(!msg.guild.channels.find('name', 'suggestions')) return msg.reply('**الرجاء إضافة روم بإسم (suggestions)**');
     let args = msg.content.split(" ").slice(1);
@@ -178,7 +178,7 @@ client.on('message', msg => {
 
       الاقتراح :
       ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
-`)
+      `)
       .then(function (message) {
         message.react('✅')
         message.react('❌')
