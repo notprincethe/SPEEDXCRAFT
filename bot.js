b8ec659669cd0db2b43dd37d11dae621bf7387a4
ELMUSAUIDK <39737898+ELMUSAUIDK@users.noreply.github.com>
2018-07-19 18:04:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2084,6 +2084,39 @@ var fkk =[
   }
 });
   
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('welcome', 'Welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SERVER NAME **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+    
+
+   client.on('message', message => {
+if (message.content.startsWith('.p-sug')){
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("random")
+  .addField(" **Done | تــــم :white_check_mark:**" , " ** تم إرسال الرسالة لصاحب البوت سنتطلع عليها قريبا  **")
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
